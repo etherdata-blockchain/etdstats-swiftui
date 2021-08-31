597e79a2b655f05385db6110f0a446c4d882ae81
sirily11 <sirily1997@gmail.com>
2021-08-31 17:12:26 +0800

    Initial Commit

---
+++ Shared/etdstatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  etdstatsApp.swift
+//  Shared
+//
+//  Created by Qiwei Li on 8/31/21.
+//
+
+import SwiftUI
+
+@main
+struct etdstatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
